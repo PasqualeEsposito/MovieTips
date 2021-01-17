4faa29e51617e7ef331d06565521360e75fece96
saverx <63292169+saverx@users.noreply.github.com>
2021-01-17 11:35:03 +0100

    aggiunta:Logout,home.

---
+++ src/main/java/control/Ricerca.java
@@ -0,0 +1,34 @@
+package control;
+
+import model.film.Film;
+import model.film.FilmDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Ricerca", urlPatterns = "/ricerca")
+public class Ricerca extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        FilmDAO filmDao= new FilmDAO();
+        List<Film> films = filmDao.doRetrieveByWord(request.getParameter("inputRicerca"));
+        request.setAttribute("films", films);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/view/ricerca.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+
+
+}
