c84dd388f3ebbb3a2a7e4e87149698efb4f1db7e
Carmine <c.ippolito2@studenti.unisa.it>
2021-01-17 09:42:21 +0100

    Aggiornato database

--- src/main/java/model/connection/ConPool.java
+++ src/main/java/model/connection/ConPool.java
@@ -1,63 +1,30 @@
 package model.connection;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.TimeZone;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 
 public class ConPool {
+    private static DataSource datasource;
 
-    private static List<Connection> freeDbConnections = new LinkedList<Connection>();
-
-    static {
-        freeDbConnections = new LinkedList<Connection>();
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    public static Connection getConnection() throws SQLException {
+        if (datasource == null) {
+            PoolProperties p = new PoolProperties();
+            p.setUrl("jdbc:mysql://localhost:3306/MovieTips?serverTimezone=" + TimeZone.getDefault().getID());
+            p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            p.setUsername("root");
+            p.setPassword("root");
+            p.setMaxActive(100);
+            p.setInitialSize(10);
+            p.setMinIdle(10);
+            p.setRemoveAbandonedTimeout(60);
+            p.setRemoveAbandoned(true);
+            datasource = new DataSource();
+            datasource.setPoolProperties(p);
         }
-
-    }
-
-    public ConPool() {
-        freeDbConnections = new LinkedList<Connection>();
-    }
-
-    private static synchronized Connection createDBConnection() throws SQLException {
-
-        String url = "jdbc:sqlserver://universita.database.windows.net:1433;database=movietipsdb;" +
-                "user=unisaStudents@universita;password=(StudentiUnisa2021);encrypt=true;" +
-                "trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-        Connection newConnection = DriverManager.getConnection(url);
-        newConnection.setAutoCommit(false);
-        return newConnection;
-    }
-
-    public static synchronized Connection getConnection() throws SQLException {
-        Connection connection;
-
-        if (!freeDbConnections.isEmpty()) {
-            connection = (Connection) freeDbConnections.get(0);
-            freeDbConnections.remove(0);
-
-            try {
-                if (connection.isClosed())
-                    connection = getConnection();
-            } catch (SQLException e) {
-                connection.close();
-                connection = getConnection();
-            }
-        } else {
-            connection = createDBConnection();
-        }
-
-        return connection;
-    }
-
-    public static synchronized void releaseConnection(Connection connection) throws SQLException {
-        if (connection != null)
-            freeDbConnections.add(connection);
+        return datasource.getConnection();
     }
 }
