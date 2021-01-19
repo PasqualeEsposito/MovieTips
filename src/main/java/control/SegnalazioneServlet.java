fed31bb75a2bfe1de0701b2b30366c48b3a29b1d
PasqualeEsposito <72381073+PasqualeEsposito@users.noreply.github.com>
2021-01-19 16:47:01 +0100

    Aggiornamento

---
+++ src/main/java/control/SegnalazioneServlet.java
@@ -0,0 +1,8 @@
+package control;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "SegnalazioneServlet", urlPatterns = "/Segnala")
+public class SegnalazioneServlet extends HttpServlet {
+}
