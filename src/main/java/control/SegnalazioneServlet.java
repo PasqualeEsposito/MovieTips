d954afc6c0220d3d7c914585120e1bfd9a4b8d5e
PasqualeEsposito <72381073+PasqualeEsposito@users.noreply.github.com>
2021-01-19 17:49:43 +0100

    Aggiornamento

--- src/main/java/control/SegnalazioneServlet.java
+++ src/main/java/control/SegnalazioneServlet.java
@@ -1,5 +1,8 @@
 package control;
 
+import model.segnalazione.Segnalazione;
+import model.segnalazione.SegnalazioneDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +13,19 @@ import java.io.IOException;
 @WebServlet(name = "SegnalazioneServlet", urlPatterns = "/Segnala")
 public class SegnalazioneServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        return;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int idRecensione = Integer.parseInt(request.getParameter("idRecensione"));
+        String usernameUtente = request.getParameter("usernameUtente");
+        int idFilm = Integer.parseInt(request.getParameter("idFilm"));
+        Segnalazione s = new Segnalazione(idRecensione, usernameUtente);
+        SegnalazioneDAO service = new SegnalazioneDAO();
+        service.doSave(s);
+
+        response.sendRedirect("/Film?id=" + idFilm);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
     }
 }
